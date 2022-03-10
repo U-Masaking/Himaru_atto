9d83297e884b1b5ed449b1eef29fd09e7a202229
kazuki <kazu.kay27@gmail.com>
2022-03-10 23:07:07 +0900

    dblist追加

--- app.py
+++ app.py
@@ -24,8 +24,6 @@ def after_request(response):
     response.headers["Pragma"] = "no-cache"
     return response
 
-    github
-
 
 # Custom filter
 app.jinja_env.filters["usd"] = usd
@@ -44,223 +42,19 @@ if not os.environ.get("API_KEY"):
     raise RuntimeError("API_KEY not set")
 
 
-@app.route("/")
+@app.route("/dblist")
 @login_required
-def index():
-    """Show portfolio of stocks"""
-    user_id = session["user_id"]
-    stocks = db.execute("SELECT symbol, company_name, price, SUM(shares) as count FROM transactions WHERE user_id=? GROUP BY symbol", user_id)
-    cash = db.execute("SELECT cash FROM users WHERE id=?", user_id)[0]["cash"]
-    
-    total = cash
-    for stock in stocks:
-        total += stock["count"] * stock["price"]
+def dblist():
+    # データベースを取得
+    databases = db.execute("SELECT * FROM databases WHERE user_id = ?", session["user_id"])
 
-    return render_template("index.html", stocks=stocks, cash=cash, total=total)
-
-
-@app.route("/buy", methods=["GET", "POST"])
-@login_required
-def buy():
-    """Buy shares of stock"""
-    if request.method == "POST":
-
-        if not request.form.get("symbol"):
-            return apology("MISSING SYMBOL", 400)
-
-        elif not request.form.get("shares"):
-            return apology("MISSING SHARES", 400)
-
-        symbol = request.form.get("symbol").upper()
-        if lookup(symbol) is None:
-            return apology("INVALID SYMBOL", 400)
-        
-        try:
-            shares = int(request.form.get("shares"))
-        except:
-            return apology("INVALID SYMBOL", 400)
-            
-        if shares <= 0:
-            return apology("INVALID SHARES", 400)
-
-        response = lookup(symbol)
-        price = response["price"]
-        total = price * shares
-        cash = db.execute("SELECT cash FROM users WHERE id=?", session["user_id"])[0]["cash"]
-
-        if cash < total:
-            return apology("CAN'T AFFORD", 400)
+    # データベースidと合致するテーブルを取得
+    for database in databases:
+        rows = db.execute("SELECT * FROM tables WHERE database_id = ?", database["id"])
+        if tables:
+            tables.append(rows)
         else:
-            db.execute("UPDATE users SET cash=? WHERE id=?", cash - total, session["user_id"])
-            db.execute("INSERT INTO transactions (user_id, symbol, company_name, shares, price, total) VALUES(?,?,?,?,?,?)"
-            , session["user_id"], response["symbol"], response["name"], shares, price, total)
+            tables = rows
 
-        return redirect("/")
-    else:
-        return render_template("buy.html")
+    return render_template("dblist.html", databases=databases, tables=tables)
 
-
-@app.route("/history")
-@login_required
-def history():
-    """Show history of transactions"""
-    user_id = session["user_id"]
-    stocks = db.execute("SELECT * FROM transactions WHERE user_id=?", user_id)
-    return render_template("history.html", stocks=stocks)
-
-
-@app.route("/login", methods=["GET", "POST"])
-def login():
-    """Log user in"""
-
-    # Forget any user_id
-    session.clear()
-
-    # User reached route via POST (as by submitting a form via POST)
-    if request.method == "POST":
-
-        # Ensure username was submitted
-        if not request.form.get("username"):
-            return apology("must provide username", 403)
-
-        # Ensure password was submitted
-        elif not request.form.get("password"):
-            return apology("must provide password", 403)
-
-        # Query database for username
-        rows = db.execute("SELECT * FROM users WHERE username = ?", request.form.get("username"))
-
-        # Ensure username exists and password is correct
-        if len(rows) != 1 or not check_password_hash(rows[0]["hash"], request.form.get("password")):
-            return apology("invalid username and/or password", 403)
-
-        # Remember which user has logged in
-        session["user_id"] = rows[0]["id"]
-
-        # Redirect user to home page
-        return redirect("/")
-
-    # User reached route via GET (as by clicking a link or via redirect)
-    else:
-        return render_template("login.html")
-
-
-@app.route("/logout")
-def logout():
-    """Log user out"""
-
-    # Forget any user_id
-    session.clear()
-
-    # Redirect user to login form
-    return redirect("/")
-
-
-@app.route("/quote", methods=["GET", "POST"])
-@login_required
-def quote():
-    """Get stock quote."""
-    if request.method == "POST":
-
-        if  not request.form.get("symbol"):
-            return apology("MISSING SYMBOL", 400)
-
-        symbol = request.form.get("symbol")
-        if lookup(symbol) is None:
-            return apology("INVALID SYMBOL", 400)
-
-        response = lookup(symbol)
-
-        return render_template("quoted.html", name=response["name"], price=response["price"], symbol=response["symbol"])
-    else:
-        return render_template("quote.html")
-
-
-@app.route("/register", methods=["GET", "POST"])
-def register():
-    """Register user"""
-    if request.method == "POST":
-
-        # Ensure username was submitted
-        if not request.form.get("username"):
-            return apology("must provide username", 400)
-
-        # Ensure password was submitted
-        elif not request.form.get("password"):
-            return apology("must provide password", 400)
-
-        # Ensure confirmation password submitted
-        elif not request.form.get("confirmation"):
-            return apology("must provide confirmation", 400)
-
-        username = request.form.get("username")
-        password = request.form.get("password")
-        confirmation = request.form.get("confirmation")
-        rows = db.execute("SELECT * FROM users WHERE username = ?", username)
-
-        if password != confirmation:
-            return apology("password didn't match")
-
-        # Add user to database
-        hash = generate_password_hash(password)
-        try:
-            db.execute("INSERT INTO users (username, hash) VALUES(?, ?)", username, hash)
-            return redirect("/")
-        except:
-            return apology("username is already taken", 400)
-
-    # User reached route via GET (as by clicking a link or via redirect)
-    else:
-        return render_template("register.html")
-
-
-@app.route("/sell", methods=["GET", "POST"])
-@login_required
-def sell():
-    """Sell shares of stock"""
-    user_id = session["user_id"]
-    
-    if request.method == "POST":
-        symbol = request.form.get("symbol")
-        shares = int(request.form.get("shares"))
-        
-        if not symbol:
-            return apology("MISSING SYMBOL", 400)
-            
-        elif not shares:
-            return apology("MISSING SHARES", 400)
-            
-        elif shares < 1:
-            return apology("INVALID SHARES", 400)
-        
-        rows = db.execute("SELECT symbol, company_name, price, SUM(shares) as count FROM transactions WHERE user_id=? AND symbol=? GROUP BY symbol", user_id, symbol)
-        
-        if len(rows) == 0:
-            return apology("INVALID SYMBOL", 400)
-            
-        elif rows[0]["count"] < shares:
-            return apology("TOO MANY SHARES", 400)
-        
-        sell = shares * rows[0]["price"]
-        
-        cash = db.execute("SELECT cash FROM users WHERE id=?", user_id)[0]["cash"]
-        db.execute("UPDATE users SET cash=? WHERE id=?", cash + sell, session["user_id"])
-        db.execute("INSERT INTO transactions (user_id, symbol, company_name, shares, price, total) VALUES(?,?,?,?,?,?)"
-        , user_id, rows[0]["symbol"], rows[0]["company_name"], -shares, rows[0]["price"], sell)
-            
-        return redirect("/")
-    else:
-        symbols = db.execute("SELECT symbol FROM transactions WHERE user_id=? GROUP BY symbol", user_id)
-        return render_template("sell.html", symbols=symbols)
-
-
-def errorhandler(e):
-    """Handle error"""
-    if not isinstance(e, HTTPException):
-        e = InternalServerError()
-    return apology(e.name, e.code)
-
-
-# Listen for errors
-for code in default_exceptions:
-    app.errorhandler(code)(errorhandler)
