ac325f033bc63bcb9c786b5da07ecd57230f5a0c
takuma817 <whalepoe817@gmail.com>
2022-03-24 22:37:30 +0900

    3/24

--- app.py
+++ app.py
@@ -1,93 +1,175 @@
-from flask import Flask, flash, render_template, session, request
+from flask import Flask, flash, render_template, session, request, redirect, url_for
 from flask_session import Session
-from database import init_db, db
+from testProject.database import init_db, db
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.orm import scoped_session, sessionmaker
-import models
+import testProject.models
 import psycopg2
 import sqlalchemy
 
-
-db = "postgresql+psycopg2://postgres:724817tf@192.168.0.26:5432/testDB"
-
-engine = sqlalchemy.create_engine(
-    db,
-    encoding="utf-8",
-    echo=True
-)
+from tempfile import mkdtemp
 
 
-def create_app():
-    app = Flask(__name__)
-    app.config.from_object('flask_sample.config.Config')
-    init_db(app)
-    return app
+app = Flask(__name__)
 
-app = create_app()
-
+app.config["SESSION_FILE_DIR"] = mkdtemp()
 app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
+app.config["TEMPLATES_AUTO_RELOAD"] = True
 Session(app)
 
-session = scoped_session(
-    sessionmaker(
-        autocommit=False,
-        autoflush=False,
-        bind=engine
-    )
-)
 
-db.create_all()
+connection = psycopg2.connect(host="localhost", database="testDB", user="postgres", password="724817tf", port=5432)
+connection.autocommit = False
+cur = connection.cursor()
 
-@app.route('/')
-def index_before():
 
+
+# DATABASE = "postgresql+psycopg2://postgres:724817tf@localhost:5432/testDB"
+
+# engine = sqlalchemy.create_engine(
+#     DATABASE,
+#     encoding="utf-8",
+#     echo=True
+# )
+
+# # app.config.from_object('testDB.config.Config')
+
+# db = SQLAlchemy(app)
+
+# session1 = scoped_session(
+#     sessionmaker(
+#         autocommit=False,
+#         autoflush=False,
+#         bind=engine
+#     )
+# )
+
+# session = session1()
+
+# db.create_all()
+
+
+
+@app.route("/", methods=["GET", "POST"])
+def home():
+    # postのときの処理
+    if request.method == "POST":
+
+        # 値の取得とエラーチェック
+        db_name = request.form.get("db_name")
+        tb_name = request.form.get("table_name")
+
+        cur.execute("SELECT name FROM databases")
+        db_list = cur.fetchall()
+
+        if db_name not in db_list[0]:
+            flash("データベースが存在しません")
+            return render_template("home.html")
+
+        cur.execute("SELECT id FROM databases WHERE name = %(db_name)s", {'db_name':db_name})
+        db_id = cur.fetchall()[0][0]
+
+        cur.execute("SELECT name FROM tables WHERE database_id = %(db_id)s" % {'db_id':db_id})
+        tb_list = cur.fetchall()
+
+        if tb_name not in tb_list[0]:
+            flash("テーブルが存在しません")
+            return render_template("home.html")
+
+        # templateが選択されたときのテーブルの複製
+        if tb_name == "template1":
+            cur.execute("create table cp_template (like template1 including all)")
+            cur.execute("insert into cp_template select * from template1")
+            cur.execute("insert into tables (name, database_id) values ('cp_template', 1)")
+            tb_name = "cp_template"
+
+        # 選択されたテーブルとデータベースの表示
+        return redirect(url_for('index_before', db_id=db_id, table1_name=tb_name))
+
+    # getのとき
+    return render_template("home.html")
+
+
+@app.route('/index_before/<int:db_id>/<string:table1_name>', methods=["GET", "POST"])
+def index_before(db_id, table1_name):
     # userID = session["user_id"]
     transaction_flag = False
 
+    cur.execute("SELECT * FROM %(name)s" % {'name':table1_name})
+    table1 = cur.fetchall()
+
+    # cur.execute("SELECT column_name AS name FROM information_schema.columns WHERE table_name = %(name)s" % {'name':table1_name})
+    table1_columns = [col.name for col in cur.description]
+
+    # postのとき
     if request.method == "POST":
 
-        query = request.form.get("Query")
+        # 終了ボタンが押されたとき
+        if request.form['send'] == 'end':
 
-        # ユーザーのテーブル全部
-        table1 = db.session.execute("SELECT * FROM ?", ユーザーが操作しているデータベース)
+            if transaction_flag == True:
+                cur.execute("ROLLBACK;")
+
+            if table1_name == "cp_template":
+                cur.execute("drop table if exists cp_template")
+
+            return redirect(url_for('home'))
+
+        query = request.form.get("Query")
 
         # 一時保存の場合
         if request.form['send'] == 'temporary':
             if transaction_flag == False:
                 transaction_flag = True
-                db.session.execute("BEGIN;")
+                cur.execute("BEGIN;")
+
             try :
-                db.session.execute(query)
-                db.session.execute(";")
+                cur.execute(query)
             except :
                 flash("SQL構文が間違っています")
-                return render_template("index_before.html")
+                return render_template("index_before.html", table1=table1, table1_columns=table1_columns, id=db_id, name=table1_name)
 
-            # ユーザーが捜査した後のテーブル全部
-            table2 = db.session.execute("SELECT * FROM ?", ユーザーが捜査しているtable)
+            # 入力がselect文のとき
+            if query[0] == 's' or query[0] == 'S':
 
-            return render_template("index_before.html", table1, table2, ユーザーが捜査した箇所（結果用）)
+                # ユーザーが捜査した後のテーブル取得
+                operated_table = cur.fetchall()
+
+            # select文以外
+            else:
+                cur.execute("select * from %(name)s" % {'name':table1_name})
+                operated_table = cur.fetchall()
+
+            o_table_columns = [col.name for col in cur.description]
+
+            return render_template("index_before.html", table1=table1, table1_columns=table1_columns, operated_table=operated_table, o_table_columns=o_table_columns, id=db_id, name=table1_name)
+
 
         # 実行した場合
         if request.form['send'] == 'execute':
             try :
-                db.session.execute(query)
-                db.session.execute(";")
+                cur.execute(query)
             except :
                 flash("SQL構文が間違っています")
-                return render_template("index_before.html")
+                return render_template("index_before.html", table1=table1, table1_columns=table1_columns, id=db_id, name=table1_name)
+
+            # 入力がselect文のとき
+            if query[0] == 's' or query[0] == 'S':
+                operated_table = cur.fetchall()
+
+            # select文以外
+            else:
+                cur.execute("select * from %(name)s" % {'name':table1_name})
+                operated_table = cur.fetchall()
+
+            o_table_columns = [col.name for col in cur.description]
 
             if transaction_flag == True:
-                db.execute("COMMIT;")
+                cur.execute("COMMIT;")
 
             flash("データベースが編集されました")
 
-            table = db.session.execute("SELECT * FROM ?", ユーザーが捜査しているtable)
+            return render_template("index_before.html", table1=table1, table1_columns=table1_columns, operated_table=operated_table, o_table_columns=o_table_columns, id=db_id, name=table1_name)
 
-            return render_template("index_before.html", table, )
-
-    if transaction_flag == True:
-        db.session.execute("ROLLBACK;")
-
-    return render_template('index_before.html')
+    return render_template('index_before.html', table1=table1, table1_columns=table1_columns, id=db_id, name=table1_name)
